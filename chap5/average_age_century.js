925b8ab13e60c85bdb6f37a93e97ad9985f1b5b7
Droper <pmunoz@gmail.com>
2015-12-28 19:00:25 -0500

    Bonus Point

--- chap5/average_age_century.js
+++ chap5/average_age_century.js
@@ -19,9 +19,43 @@ function century_died(person){
   }
 }
 
+
 ancestry.forEach(century_died);
 
 // Calculates the average for each century and print it.
+for (var century in person_centuries){
+  person_centuries[century] = average(person_centuries[century]);
+  console.log(century + ": " + person_centuries[century]);
+}
+
+/////////////////////BONUS POINT////////////////////
+
+// Function that generalizes the grouping by acepting an array and
+// a group function and brouping the values by the name.
+function groupBy(array, group_function){
+  var person_ages = {}
+  for (item in array){
+    var result = group_function(item);
+    if (result.name in person_ages){
+      person_ages[result.name].push(result.value);
+    }
+    else{
+      person_ages[result.name] = [result.value];
+    }
+  }
+  return person_ages;
+}
+
+// Group function that returns an object with two attributes
+// The name that is the grouping key and the value
+function century_died2(person){
+  return {name: Math.ceil(person.died/100),
+          value: person.died - person.born};
+}
+
+
+person_centuries = groupBy(ancestry, century_died2);
+
 for (var century in person_centuries){
   person_centuries[century] = average(person_centuries[century]);
   console.log(century + ": " + person_centuries[century]);
